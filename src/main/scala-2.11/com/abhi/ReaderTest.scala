462372b2d27a1395516ef4cafd542337c88b42a1
abhsrivastava <abhsrivastava@researchnow.com>
2017-01-21 09:49:30 -0600

    use readers in monadic expressions

--- src/main/scala-2.11/com/abhi/ReaderTest.scala
+++ src/main/scala-2.11/com/abhi/ReaderTest.scala
@@ -6,15 +6,27 @@ package com.abhi
 import cats.data.Reader
 import cats.syntax.applicative._
 object ReaderTest extends App {
-   type DbReader2[A] = Reader[Map[Int, String], A]
-   val input = Map(1 -> "foo")
-   val output = findUser(1).run(input)
-   println(output.getOrElse("not found"))
-   def findUser(x: Int) : DbReader2[Option[String]] = {
+   type DbReader2[A] = Reader[Db, A]
+   val input = Db(Map("foo" -> 1), Map(1 -> "bar"))
+   val output = checkLogin("foo", "baz").run(input)
+   println(output)
+
+   def findUserId(name: String) : DbReader2[Option[Int]] = {
       Reader(db =>
-         db.get(x)
+         db.userNames.get(name)
       )
+   }
+   def checkPassword(userId: Int, password: String) : DbReader2[Boolean] = {
+      Reader(db =>
+         db.password.get(userId).contains(password)
+      )
+   }
+   def checkLogin(userName: String, password: String) : DbReader2[Boolean] = {
+      for {
+         userId <- findUserId(userName)
+         passwordOk <- userId.map(userId => checkPassword(userId, password)).getOrElse(false.pure[DbReader2])
+      } yield passwordOk
    }
 }
 
-case class Db (userNames: Map[String, String], password: Map[String, String])
+case class Db (userNames: Map[String, Int], password: Map[Int, String])
